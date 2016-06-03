9658d3387fa88f348c330a6d09c057bf41f35e97
Lea Hayes <lea@rotorz.com>
2016-06-03 04:09:26 +0100

    Remove dependency on 'babel' by using CommonJS.

--- test/unit/JsonHandler.js
+++ test/unit/JsonHandler.js
@@ -1,11 +1,13 @@
 // Copyright (c) Rotorz Limited. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
+"use strict";
 
-import given from "mocha-testdata";
-import should from "should";
 
-import {JsonHandler} from "../../lib/JsonHandler";
+const given = require("mocha-testdata");
+const should = require("should");
+
+const JsonHandler = require("../../lib/JsonHandler").JsonHandler;
 
 
 describe("JsonHandler", function () {
